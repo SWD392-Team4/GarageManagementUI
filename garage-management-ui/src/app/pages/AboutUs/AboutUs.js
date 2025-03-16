ba194d3ce56229d2bdac09f22dc02d0ca17611d5
Giang Nguyen Thi Huong <nguyenthihuonggiangthth@gmail.com>
2025-03-16 17:52:51 +0700

    Update translation

--- garage-management-ui/src/app/pages/AboutUs/AboutUs.js
+++ garage-management-ui/src/app/pages/AboutUs/AboutUs.js
@@ -6,14 +6,16 @@ import Footer from "../Home/partials/Footer/Footer";
 import QualitySection from "./partials/Quality";
 import FeatureSection from "./partials/Features";
 import EngineerSlider from "./partials/EngineerSlider";
+import { useTranslation } from "react-i18next";
 export default function AboutUs() {
+  const { t } = useTranslation("about_title");
   return (
     <>
       <PageTitle
         image={commpagesbg}
-        title="About Us"
-        title1="Home"
-        subtitle="Aboutus"
+        title={t("about_title.title")}
+        title1={t("about_title.breadcrumb_home")}
+        subtitle={t("about_title.title")}
         height="500"
       />
 
